fc28ac26868885fe4d1028c0d12dd7beabb59a44
SasquatchShawn <srmiranda1@gmail.com>
2015-08-12 14:00:14 -0400

    Extra credit done

--- server.rb
+++ server.rb
@@ -14,5 +14,10 @@ get "/teams/:team_name" do
   erb :show, locals: { teams: teams, team_name: params[:team_name] }
 end
 
+get "/positions/:position" do
+  teams = data
+  erb :positions, locals: { teams: teams, position: params[:position]}
+end
+
 set :views, File.join(File.dirname(__FILE__), "views")
 set :public_folder, File.join(File.dirname(__FILE__), "public")
